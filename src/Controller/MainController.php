3f580849fed22f58753a0abb275af2bbf9365b2e
CherrySmejkal <thanassawans@gmail.com>
2022-12-12 12:33:19 +0100

    Create Main Controller and finish gallery

---
+++ src/Controller/MainController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/main', name: 'app_main')]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/gallery', name: 'app_gallery')]
+    public function gallery(): Response
+    {
+        return $this->render('main/gallery.html.twig', [
+            'controller_name' => 'CoursesController',
+        ]);
+    }
+}
